edf0ee476c2cefe333e1757b2b7c19ec00cc8f38
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-08-29 01:56:18 +0530

    conditional redirects

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// let reqCount = 0;
+// export function middleware(req: NextRequest) {
+//     reqCount++;
+//     const res = NextResponse.next();
+//     console.log("Request count: ", reqCount);
+//     return res;
+// }
+
+export function middleware(req: NextRequest) {
+    console.log(req.nextUrl.pathname);
+    if (req.nextUrl.pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/signin", req.url));
+    }
+
+    if (req.nextUrl.pathname.startsWith("/dashboard")) {
+        return NextResponse.next();
+    }
+}
+// export const config = {
+//     matcher: "/api/:path*",
+// };
